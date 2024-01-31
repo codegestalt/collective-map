b5fdf18e5b18495ad4db9b3524cf367450363575
Rodrigo Haenggi <rodrigo@codegestalt.com>
2024-01-31 09:16:03 +0100

    add zoom function

--- public/js/collective_map.js
+++ public/js/collective_map.js
@@ -97,6 +97,25 @@ export class CollectiveMap {
         this.map.selectAll('g').attr('transform', event.transform);
       });
 
+    let svg = d3.select("svg")
+      .attr("width", "100%")
+      .attr("height", "100%")
+      .style("background-color", "#eeeeee")
+      .call(zoom)
+      .on("dblclick.zoom", null);
+
+    let g = svg.append("g");
+    d3.select("#zoom_in").on("click", function() {
+      zoom.scaleBy(svg.transition().duration(750), 1.2);
+    });
+    d3.select("#zoom_out").on("click", function() {
+      zoom.scaleBy(svg.transition().duration(750), 0.8);
+    });
+
+    function zoomed() {
+        g.attr("transform", d3.event.transform);
+    }
+
     const centerX = this.width / 3.0;
     const centerY = this.height / 2.5;
 
